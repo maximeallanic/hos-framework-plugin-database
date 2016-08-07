fd1c2d209457a278938e6b9b32a3b2e062ab1b79
Maxime Allanic <maxime.allanic@daehl.com>
2016-08-07 14:13:28 +0200

    [FIX] Composer.json

---
+++ src/Events.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Hos\Plugin\Database;
+
+/**
+  * @author Maxime Allanic maxime@allanic.me
+  * @license GPL
+  * @internal Created 2016-07-11 14:39:21
+  */
+class Events{
+	/**
+	 * [remove description]
+	 * @param  Entity | int $entity
+	 * @return [type]         [description]
+	 */
+	static function remove($entity) {
+
+	}
+
+	static function create($entity) {
+
+	}
+
+	static function get($id) {
+
+	}
+
+	static function getAll($filters) {
+
+	}
+}
